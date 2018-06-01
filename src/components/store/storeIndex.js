f2a7d43a17e571cbee19a39d066eb3a8790d7962
kiscon <kiscon@163.com>
2018-06-01 22:51:06 +0800

    添加代理

--- src/components/store/storeIndex.js
+++ src/components/store/storeIndex.js
@@ -1,8 +1,51 @@
 import React from 'react';
+import fetch from '../../server/fetch'
 
 import '../../assets/css/store/store.css'
 
 export default class StoreIndex extends React.Component {
+    //初始化
+    constructor() {
+        super();
+        this.state = {
+            data:{}
+        }
+    }
+
+    //第一次渲染结束
+    componentDidMount() {
+        console.log(123);
+        // this.getData('/api/productSve/getProducts')
+        fetch.getFetch('/api/productSve/getProducts')
+            .then(data => {
+                console.log(data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+    }
+
+    getData(url) {
+        this.getFetch(url)
+			.then(data => {
+				console.log(data)
+			})
+			.catch(err => {
+				console.log(err)
+			})
+	}
+
+    getFetch(url) {
+        return fetch(url).then(res => {
+            if (res.status !== 200) {
+                console.log('Looks like there was a problem. Status Code: ' + res.status)
+                return
+            }
+            console.log(res)
+            return res.json()
+        })
+    }
+
 	render() {
 		return (
 			<div className="shop">
--- src/server/httpService.js
+++ src/server/httpService.js
@@ -5,7 +5,7 @@ let url='https://api.douban.com/v2';
 export  default {
     //暴露获取电影列表方法
     //type 电影列表类型
-    getMovieList:(type,page,count)=>{
+    getMovieList(type,page,count) {
         //拼接url https://api.douban.com/v2/movie/in_theaters?page=1
         //获取到start 启示数
         let start=(page-1)*count;
@@ -13,13 +13,13 @@ export  default {
         //根据url 发送数据请求
         return fetchJsonp(_url,{timeout:8000}).then(r=>r.json());
     },
-    getMovieInfo:(id)=>{
+    getMovieInfo(id) {
         //拼接url https://api.douban.com/v2/movie/subject/17
         let _url=`${url}/movie/subject/${id}`;
         //根据url 发送数据请求
         return fetchJsonp(_url,{timeout:8000}).then(r=>r.json());
     },
-    getSearchInfo:(q,page,count)=>{
+    getSearchInfo(q,page,count) {
         let start=(page-1)*count;
         let _url=`${url}/movie/search?q=${q}&start=${start}&count=${count}`;
         return fetchJsonp(_url,{timeout:8000}).then(r=>r.json());
